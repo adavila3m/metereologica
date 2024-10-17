f65efe5845b749b74dd75cbc8c7cc29dfc40dfd8
root <root@ip-172-31-36-163.ec2.internal>
2024-10-17 15:56:01 -0300

    se elimina el modelo sensordata del admin

--- networks/autogestion/admin.py
+++ networks/autogestion/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from autogestion.models import SensorData, sitio, medicion
 
 # Register your models here.
-admin.site.register(SensorData)
+#admin.site.register(SensorData)
 admin.site.register(sitio)
 admin.site.register(medicion)
